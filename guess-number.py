0bd4909fe045368a3b9fb4bed36d949668071c04
Patrick <messager.patrick428@gmail.com>
2021-06-19 23:11:07 +0200

    +gitignore

--- guess-number.py
+++ guess-number.py
@@ -2,16 +2,25 @@ import random
 
 # import time
 
-alea = random.randint(1, 100)  # Nombre aléatoire entre 0 et 100
-answer = 0
-coup = 0
-while answer != alea:
-    answer = int(input("Entrez un chiffre"))
-    if answer > alea:
-        print("Choisissez plus petit")
-        coup += 1
-    elif answer < alea:
-        print("Choisissez plus grand")
-        coup += 1
-    else:
-        print(f"Vous avez trouvé ! Vous avez mis {coup + 1} coups pour trouver")
+alea = random.randint(1, 5)  # Nombre aléatoire entre 1 et 100
+
+
+def tirage():
+    answer = 0
+    coup = 0
+    while answer != alea:
+        answer = int(input("Entrez un chiffre"))
+        if answer > alea:
+            print("Choisissez plus petit")
+            coup += 1
+        elif answer < alea:
+            print("Choisissez plus grand")
+            coup += 1
+        else:
+            print(f"Vous avez trouvé ! Vous avez mis {coup + 1} coups pour trouver")
+
+
+while choice != "exit":
+
+
+tirage()
